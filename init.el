4d143584a8bdf0d3a6f8ff042b7147b66c290bbd
Joar von Arndt <88452361+JanJoar@users.noreply.github.com>
2023-07-19 00:54:32 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
+;;; Crabmacs --- A fully functioning Gnu Emacs system
+
+;;; Commentary: This file simply serves to load other Emacs lisp files in order to neatly separate different concepts
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/packages.org"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;; init.el ends here
