0e77d5c44523354eff5f07f8eb6c7e1bfc277591
Joar von Arndt <88452361+JanJoar@users.noreply.github.com>
2023-08-08 00:53:55 +0200

    License in early-init.el
    Adds the AGPL license clause in early-init.el

--- early-init.el
+++ early-init.el
@@ -1,4 +1,18 @@
-;;; Crabmacs Early Init --- A fully functioning Gnu Emacs system
+;    Crabmacs --- A fully functioning Gnu Emacs system
+;    Copyright (C) 2023  Joar von Arndt
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU Affero General Public License as published
+;    by the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU Affero General Public License for more details.
+;
+;    You should have received a copy of the GNU Affero General Public License
+;    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary: Early optimizations mostly for improved startup times. A not-insignificant parts of are taken from https://github.com/Stefanomarton/DotFiles/ and his wonderful improvements.
 
@@ -26,6 +40,5 @@
 (tab-bar-mode -1)
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
-
 
 ;;; early-init.el ends here
