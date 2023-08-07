c451c3741fe8d45106bdc3b2fb6122a99421772e
Joar von Arndt <88452361+JanJoar@users.noreply.github.com>
2023-08-08 00:52:11 +0200

    secret.org and License for init.el
    Adds the new secret.org file into the init cycle, configuring information that you might not want public, like the settings for Gnus.

--- init.el
+++ init.el
@@ -1,11 +1,25 @@
-;;; Crabmacs --- A fully functioning Gnu Emacs system
+;    Crabmacs --- A fully functioning Gnu Emacs system
+;    Copyright (C) 2023  Joar von Arndt
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU Affero General Public License as published
+;    by the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU Affero General Public License for more details.
+;
+;    You should have received a copy of the GNU Affero General Public License
+;    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary: This file simply serves to load other Emacs lisp files in order to neatly separate different concepts
 
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/packages.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
+(org-babel-load-file (expand-file-name "~/.emacs.d/packages.org")) ;; Loads any needed packages.
+(org-babel-load-file (expand-file-name "~/.emacs.d/secret.org")) ;; User-unique information (like E-mail address and full name) that you might not want to share openly. Empty by default.
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")) ;; The main configuration file, running commands, setting keybinds, and configuring packages.
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
