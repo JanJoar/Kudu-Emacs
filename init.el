a8012095278a8e1fc1b8eef28eca10f738b09009
Joar von Arndt <joarxpablo@protonmail.com>
2023-09-21 00:04:28 +0200

    Adds solaire mode, flycheck, and compilation of Elisp files.

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
                                         ;    Kudu --- A fully functioning GNU Emacs system
                                         ;    Copyright (C) 2023  Joar von Arndt
                                         ;
+                                        ;
                                         ;    This program is free software: you can redistribute it and/or modify
                                         ;    it under the terms of the GNU General Public License as published by
                                         ;    the Free Software Foundation, either version 3 of the License, or
@@ -17,7 +18,13 @@
 ;;; Commentary: This file simply serves to load other Emacs lisp files in order to neatly separate different concepts
 
 
-(setq load-prefer-newer t) ;; Loads the newer file if one exists. This means emacs will prioritise files with newer changes.
+(setq load-prefer-newer t) ;; Loads the newer file if one exists. This means emacs will prioritise files with newer changes. 
+(use-package auto-compile  ;; Compiles Emacs-lisp files to improve startup. The setting above makes sure that new changed files take priority over compiled files. 
+  :ensure t
+  :config
+  (auto-compile-on-load-mode 1)
+  (auto-compile-on-save-mode 1))
+
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/packages.org")) ;; Loads any needed packages.
 (org-babel-load-file (expand-file-name "~/.emacs.d/secret.org")) ;; User-unique information (like E-mail address and full name) that you might not want to share openly. Empty by default.
