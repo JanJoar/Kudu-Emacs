40dfa05ea1211d42822feb3cc191cf5035f4ff07
Jovo <jovo@Nile.Nile.hem>
2023-10-17 16:08:09 +0200

    Adds text logo for terminal and easy switching for GUI.

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 
 (setq load-prefer-newer t) ;; Loads the newer file if one exists. This means emacs will prioritise files with newer changes. 
 
+(defvar Kudu-gui-logo)
 (shell-command "touch ~/.emacs.d/secret.org")
 (org-babel-load-file (expand-file-name "~/.emacs.d/secret.org")) ;; User-unique information (like E-mail address and full name) that you might not want to share openly. Empty by default. Since the file is not included in the Kudu repo it has to be created using touch in order to be loaded.
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")) ;; The main configuration file, running commands, setting keybinds, and configuring packages.
