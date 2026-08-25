ddede8098f92154e92e9876fc7a5f9060b07c982
Joar von Arndt <joarxpablo@vonarndt.se>
2026-08-26 00:22:51 +0800

    Fix for *-bar-mode

--- early-init.el
+++ early-init.el
@@ -37,10 +37,10 @@
 (setq package-quickstart t)
 
 ;; Disabling these things here prevents them from ever loading.
-(if (display-graphic-p)
-    (progn (scroll-bar-mode -1)
-           (tool-bar-mode -1)
-           (tab-bar-mode -1)))
+(unless (display-graphic-p)
+  (progn (scroll-bar-mode -1)
+         (tool-bar-mode -1)
+         (tab-bar-mode -1)))
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
 
