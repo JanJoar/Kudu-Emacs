567e390a738b4589285f41fa87dca7fce25e2fbf
Joar von Arndt <joarxpablo@vonarndt.se>
2026-08-25 21:32:56 +0800

    Fixes the removal of scroll-, tool-, and tab-bar-mode

--- early-init.el
+++ early-init.el
@@ -37,10 +37,10 @@
 (setq package-quickstart t)
 
 ;; Disabling these things here prevents them from ever loading.
-(unless (display-graphic-p)
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1)
-  (tab-bar-mode -1))
+(if (display-graphic-p)
+    (progn (scroll-bar-mode -1)
+           (tool-bar-mode -1)
+           (tab-bar-mode -1)))
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
 
